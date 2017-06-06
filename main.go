6e7a371198fbd7bbc84ffc230e9dafd32e493cd1
Sergey Parshukov <codedby@bugman.me>
2017-06-06 23:55:27 +0300

    Wrapped Quartz windows info, finding target window id

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ package main
 */
 import "C"
 import (
-	"fmt"
+	"errors"
 	"image"
 	"image/png"
+	"log"
 	"os"
 	"time"
 	"unsafe"
@@ -18,7 +19,7 @@ import (
 
 func takeScreenShot() image.Image {
 	const flags = C.kCGWindowImageDefault | C.kCGWindowImageShouldBeOpaque | C.kCGWindowImageNominalResolution
-	screenShot := C.CGWindowListCreateImage(C.CGRectInfinite, C.kCGWindowListOptionOnScreenOnly, C.kCGNullWindowID, flags)
+	screenShot := C.CGWindowListCreateImage(C.CGRectNull, C.kCGWindowListOptionOnScreenOnly, C.kCGNullWindowID, flags)
 
 	rawBytes := C.CGDataProviderCopyData(C.CGImageGetDataProvider(screenShot))
 	pointer := C.CFDataGetBytePtr(rawBytes)
@@ -29,8 +30,6 @@ func takeScreenShot() image.Image {
 	height := int(C.CGImageGetHeight(screenShot))
 	bytesPerRow := int(C.CGImageGetBytesPerRow(screenShot))
 
-	fmt.Println(C.CGImageGetBitmapInfo(screenShot))
-
 	// manually fix BGRA -> RGBA
 	for i := 0; i < length; i += 4 {
 		pixels[i], pixels[i+2] = pixels[i+2], pixels[i]
@@ -39,11 +38,91 @@ func takeScreenShot() image.Image {
 	return &image.RGBA{Pix: pixels, Stride: bytesPerRow, Rect: image.Rect(0, 0, width, height)}
 }
 
+type CFString struct {
+	ptr *C.struct___CFString
+}
+
+func WrapCFString(pointer unsafe.Pointer) CFString {
+	return CFString{(*C.struct___CFString)(pointer)}
+}
+
+func (str CFString) String() string {
+	cstring := (*C.char)(C.CFStringGetCStringPtr(str.ptr, C.kCFStringEncodingUTF8))
+	return C.GoString(cstring)
+}
+
+type CFDictionary struct {
+	ptr *C.struct___CFDictionary
+}
+
+func WrapCFDictionary(pointer unsafe.Pointer) CFDictionary {
+	return CFDictionary{(*C.struct___CFDictionary)(pointer)}
+}
+
+func (dict CFDictionary) ObjectForKey(key C.CFStringRef) unsafe.Pointer {
+	ptr := C.CFDictionaryGetValue(dict.ptr, unsafe.Pointer(key))
+	return ptr
+}
+
+func (dict CFDictionary) StringForKey(key C.CFStringRef) string {
+	ptr := dict.ObjectForKey(key)
+	return WrapCFString(ptr).String()
+}
+
+func (dict CFDictionary) IntForKey(key C.CFStringRef) int {
+	ptr := dict.ObjectForKey(key)
+	var number int
+	C.CFNumberGetValue((*C.struct___CFNumber)(ptr), C.kCFNumberIntType, unsafe.Pointer(&number))
+	return number
+}
+
+func (dict CFDictionary) CGRectForKey(key C.CFStringRef) C.CGRect {
+	dictRepresentation := WrapCFDictionary(dict.ObjectForKey(key))
+	var rect C.CGRect
+	C.CGRectMakeWithDictionaryRepresentation(dictRepresentation.ptr, &rect)
+	return rect
+}
+
+const targetAppTitle = "Code"
+
+type windowMeta struct {
+	id     int
+	title  string
+	bounds C.CGRect
+}
+
+func findWindow() (windowMeta, error) {
+	windows := C.CGWindowListCopyWindowInfo(C.kCGWindowListOptionOnScreenOnly, C.kCGNullWindowID)
+	var i C.CFIndex
+	var info windowMeta
+	for i = 0; i < C.CFArrayGetCount(windows); i++ {
+		window := WrapCFDictionary(C.CFArrayGetValueAtIndex(windows, i))
+		owner := window.StringForKey(C.kCGWindowOwnerName)
+		if owner == targetAppTitle {
+			info.title = window.StringForKey(C.kCGWindowName)
+			info.id = window.IntForKey(C.kCGWindowNumber)
+			info.bounds = window.CGRectForKey(C.kCGWindowBounds)
+			return info, nil
+		}
+	}
+	return info, errors.New("Window not found")
+}
+
+func saveImage(img image.Image) {
+	outFile, _ := os.Create("test.png")
+	png.Encode(outFile, img)
+}
+
 func main() {
 	t := time.Now()
-	screenShot := takeScreenShot()
-	fmt.Println(time.Since(t))
+	// C.wrappedListWindows()
+	winMeta, err := findWindow()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(winMeta)
+	screenShot := takeScreenShot() // C.kCGNullWindowID
+	log.Println(time.Since(t))
 
-	outFile, _ := os.Create("test.png")
-	png.Encode(outFile, screenShot)
+	saveImage(screenShot)
 }
